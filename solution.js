de43623f0c923752e619db86ecf38cdeeaafcd6d
Marcelearn <jaworskimarcel@gmail.com>
2022-07-06 11:56:33 +0200

    ersteTeil

--- solution.js
+++ solution.js
@@ -1,1 +1,23 @@
-
+// 1.
+const euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+const secondCity = euroCities[1];
+console.log(secondCity);
+//2.
+euroCities[0] = "Berlin";
+console.log(euroCities);
+//3.
+console.log(euroCities.length);
+//4.
+euroCities.pop();
+console.log(euroCities);
+//5.
+euroCities.push("Budapest");
+console.log(euroCities);
+//6.
+const asianCities = ["Singapur", "Tokio","Yakarta", "Delhi", "Seul", "Pekin", "Shangai"]; 
+//7. 
+const worldCities = euroCities.concat(asianCities);
+console.log(worldCities);
+//8.
+worldCities.reverse()
+console.log(worldCities);
